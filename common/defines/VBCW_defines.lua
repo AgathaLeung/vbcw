fdea2999b49b59c4ec51fc6aae0c24f5cd0758b8
AgathaLeung <40522701+AgathaLeung@users.noreply.github.com>
2020-02-01 22:29:08 +0800

    Initial commit

---
+++ common/defines/VBCW_defines.lua
@@ -0,0 +1,28 @@
+NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 0.5
+
+NDefines.NProduction.BASE_FACTORY_SPEED = 2
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 2
+NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 2
+
+NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 50
+NDefines.NBuildings.RADAR_RANGE_MIN = 150
+NDefines.NBuildings.RADAR_RANGE_MAX = 1500
+
+NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.75
+
+NDefines.NAir.AIR_WING_FLIGHT_SPEED_MULT = 0.3
+NDefines.NAir.AIR_WING_MAX_SIZE = 50
+NDefines.NAir.ACE_WING_SIZE = 12
+
+NDefines.NNavy.NAVAL_SPEED_MODIFIER = 1.5
+NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 1.8
+
+
+NDefines_Graphics.NGraphics.VICTORY_POINT_LEVELS = 2
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20}
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {150, 450, 1200}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {300, 500, 1500}
+
+NDefines_Graphics.NGraphics.GMT_OFFSET = 3375
+
+--  NDefines_Graphics.NFrontend.FRONTEND_POS_Y = 3000
